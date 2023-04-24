742ceec64af9e61069f2ef25323576e2e65b518e
so265 <undia42@gmail.com>
2023-04-25 01:18:09 +0200

    bulletPoints

--- assets/script.js
+++ assets/script.js
@@ -5,53 +5,42 @@ const slides = [    //Ceci, correspond à un tableau car il y a les crochets []
 	},
 	{
 		"image": "slide2.jpg",
-		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
 	},
 	{
 		"image": "slide3.jpg",
-		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>",
 	},
 	{
 		"image": "slide4.png",
-		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>",
+	},
+];
 
-//Je déclare mes variables, j'utilise des constantes, car elles nes changeront plus
+//Je déclare mes variables, j'utilise des constantes, car elles ne changeront plus, ce sont des variables non évolutives
 
 
-const leftArrow = document.getElementById("left_arrow")
+const leftArrow = document.getElementById("left_arrow") //Je vais chercher mes id(mes fléches) dans index.httml, fléche gauche et fléche droite
 const rightArrow = document.getElementById("right_arrow")
 
-
-leftArrow.addEventListener("click",() => { console.log("event listener, fléche gauche");} ) //Je mets un addEventListener au click et je verifie si cela foctionne aà l'aide console.log
-rightArrow.addEventListener("click",() => {alert("event listener, fléche droite");}) //Je mets un addEventListener au click et je verifie si cela foctionne avec une alert
-
+leftArrow.addEventListener("click", () => { console.log("event listener, fléche gauche"); }) //Je mets un addEventListener au click et je verifie si cela foctionne aà l'aide console.log
+rightArrow.addEventListener("click", () => { alert("event listener, fléche droite"); }) //Je mets un addEventListener au click et je verifie si cela foctionne avec une alert
 
 
 
 
 
 
+//J'ajoute les points pour les 4 images du slider
+function bulletPoints() {
+	const points = document.createElement("div");  //je cree la div dans laquelle seront les points, cette div sera dans la div class="dots dans index.html"
+    points.classList.add("dot"); // classList.add me permet d'aller chercher la class dot dans style.css qui correspond à mes points, dc j'ajoute mes points à ma div crée au dessus
+	const dots = document.querySelector(".dots"); //Je vais chercher ds index.html ma div qui s'appelle dots dans laquelle seront mes points et ma nouvelle div crée en ligne 37 (attention au point car c'est une class)
+	dots.appendChild(points); //la propriété appenChild() va ajouter un élément parmis les enfant d’un élément parents spécifié (l’élément ajouté par appenChild se positionnera à la fin des enfants de l’élément parent).
+	
+  }
+
+// Crée un bullet point pour chaque élément de mon tableau d'images
+slides.forEach(bulletPoints);
 
 
-
-
-
-
-
-
-
-
-
-
-//const banner = document.getElementById("banner");
-//const slides = document.getElementById("slides");
-
-//const left = document.getElementById("left");
-//left.addEventListener("click", function () {
-//	slides(+1);
-//},
-//)
-
-//console.log("left");
